80641017b645224f06a03dcf34b409d8cb89231f
Dante <82224229+DanteSingh@users.noreply.github.com>
2023-09-16 19:43:17 +0530

    Instructions SFX And BG music

---
+++ Assets/Scripts/audioManager.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class audioManager : MonoBehaviour
+{
+  public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+  
+}
