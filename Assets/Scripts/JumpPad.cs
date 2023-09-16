6eed388af14c560ae37d90902b5ac6c44b4a2991
Dante <82224229+DanteSingh@users.noreply.github.com>
2023-09-17 00:01:22 +0530

    Jump

--- Assets/Scripts/JumpPad.cs
+++ Assets/Scripts/JumpPad.cs
@@ -5,10 +5,16 @@ using UnityEngine.Rendering;
 
 public class JumpPad : MonoBehaviour
 {
+	public AudioSource bounce;
 	public float JumpForce = 5;
 	private void OnTriggerEnter2D(Collider2D collision)
 	{
 		if (collision.attachedRigidbody != null)
 			collision.attachedRigidbody.AddForce(transform.up * JumpForce, ForceMode2D.Impulse);
+
+		if (bounce != null)
+            {
+                bounce.Play();
+            }
 	}
 }
